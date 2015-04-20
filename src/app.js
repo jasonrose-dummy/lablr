30a6673bf100ddaab19054d7a22e9c12ee5629a7
Jason Rose <jrose@shutterfly.com>
2015-04-20 11:15:01 -0700

    commit on Mon Apr 20 11:15:01 MST 2015

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log('hello');
+var React = require('react');
+
+var Hello = React.createClass({
+  render: function() {
+    return <h1>Hello from react</h1>;
+  }
+});
+
+console.log('Hello');
+
+React.render(<Hello/>, document.body);
